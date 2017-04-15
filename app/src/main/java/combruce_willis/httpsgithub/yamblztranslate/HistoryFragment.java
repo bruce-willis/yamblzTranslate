f116753d3584b80dc7dde1f4c7ed9120144d0fac
Yury Belousov <yury-belousov@yandex.ru>
2017-04-15 21:55:20 +0300

    implement fragment classes

---
+++ app/src/main/java/combruce_willis/httpsgithub/yamblztranslate/HistoryFragment.java
@@ -0,0 +1,23 @@
+package combruce_willis.httpsgithub.yamblztranslate;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yury- on 4/15/2017.
+ */
+
+public class HistoryFragment extends Fragment {
+    public HistoryFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+}
