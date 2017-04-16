d02f4a9afb95a8e8ccbfaac812b8fce97cd91093
Yury Belousov <yury-belousov@yandex.ru>
2017-04-17 01:47:03 +0300

    add done button

--- app/src/main/java/combruce_willis/httpsgithub/yamblztranslate/TranslateFragment.java
+++ app/src/main/java/combruce_willis/httpsgithub/yamblztranslate/TranslateFragment.java
@@ -3,9 +3,13 @@ package combruce_willis.httpsgithub.yamblztranslate;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +27,10 @@ public class TranslateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translate, container, false);
+        View view =  inflater.inflate(R.layout.fragment_translate, container, false);
+        EditText editText = (EditText)  view.findViewById(R.id.Translation);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        return view;
     }
 
 }
