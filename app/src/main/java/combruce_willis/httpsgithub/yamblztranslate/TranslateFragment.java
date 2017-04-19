96f4e8ae95e8e4e1b2526e2fdcd91c5d772c14c7
Yury Belousov <yury-belousov@yandex.ru>
2017-04-19 23:12:51 +0300

    Add `Done` action button in keyboard (#4)
    * add done button
    
    * add onEditorAction method
    
    * Save state after changing orientation

--- app/src/main/java/combruce_willis/httpsgithub/yamblztranslate/TranslateFragment.java
+++ app/src/main/java/combruce_willis/httpsgithub/yamblztranslate/TranslateFragment.java
@@ -3,27 +3,45 @@ package combruce_willis.httpsgithub.yamblztranslate;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TranslateFragment extends Fragment {
+public class TranslateFragment extends Fragment implements TextView.OnEditorActionListener {
 
 
     public TranslateFragment() {
         // Required empty public constructor
     }
-
+    private TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translate, container, false);
+        View view =  inflater.inflate(R.layout.fragment_translate, container, false);
+        textView = (TextView) view.findViewById(R.id.editText);
+        EditText editText = (EditText) view.findViewById(R.id.Translation);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setOnEditorActionListener(this);
+        return view;
     }
 
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_DONE)
+        {
+            textView.setText("all is ok");
+        }
+        return false;
+    }
 }
