08187183d98d8222bfbacbc7bde1f88d351ad4a5
Yury Belousov <yury-belousov@yandex.ru>
2017-04-21 01:28:41 +0300

    add search and delete icons

--- app/src/main/java/combruce_willis/httpsgithub/yamblztranslate/View/FavoriteFragment.java
+++ app/src/main/java/combruce_willis/httpsgithub/yamblztranslate/View/FavoriteFragment.java
@@ -4,6 +4,8 @@ package combruce_willis.httpsgithub.yamblztranslate.View;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,11 +22,17 @@ public class FavoriteFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_items, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_history, container, false);
     }
 
